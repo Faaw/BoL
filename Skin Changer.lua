056955baefbf3f8436b55227a60f1d3cf0f72d29
Faaw <mokilolilol@gmail.com>
2018-01-27 20:18:33 +0100

    Add files via upload

---
+++ Skin Changer.lua
@@ -0,0 +1,20 @@
+function OnLoad()
+Menu()
+end
+function Menu()
+menu = scriptConfig("Skin Changer", "Skin Changer")
+menu:addParam("skin", "Enable", SCRIPT_PARAM_ONOFF, true)
+menu:addParam("skinlist", "Skin", SCRIPT_PARAM_LIST, 1, {"1","2","3","4","5","6","7","8","9","10","11","12","13"})
+menu:setCallback("skin", function (value)
+if value then
+SetSkin(myHero, menu.skinlist-1)
+else
+SetSkin(myHero, -1)
+end
+end)
+menu:setCallback("skinlist", function (value)
+if menu.skin then
+SetSkin(myHero, menu.skinlist-1)
+end
+end)	
+end
